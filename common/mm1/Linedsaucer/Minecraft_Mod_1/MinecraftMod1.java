ddc978611131d0018c3816540ab47cd389637017
thunderjerre <jeroen.schelkens@telenet.be>
2013-04-20 18:30:04 +0200

    0.0.1

---
+++ common/mm1/Linedsaucer/Minecraft_Mod_1/MinecraftMod1.java
@@ -0,0 +1,27 @@
+package mm1.Linedsaucer.Minecraft_Mod_1;
+
+import mm1.Linedsaucer.Minecraft_Mod_1.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class MinecraftMod1 {
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+    @Init
+    public void Init(FMLInitializationEvent event){
+        
+    }
+    @PostInit
+    public void PostInit(FMLPostInitializationEvent event){
+        
+    }
+}
---
+++ common/mm1/Linedsaucer/Minecraft_Mod_1/lib/Reference.java
@@ -0,0 +1,8 @@
+package mm1.Linedsaucer.Minecraft_Mod_1.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "mm1";
+    public static final String MOD_NAME = "Minecraft Mod 1";
+    public static final String VERSION = "0.0.1";
+}
